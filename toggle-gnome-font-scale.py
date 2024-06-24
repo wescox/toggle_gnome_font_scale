1a0fc9386f3f11181f3356c586bf2b425ec55189
Wes Cox <wesley.cox@servicetrade.com>
2024-06-24 14:26:05 -0600

    got dynamic icon working

--- toggle-gnome-font-scale.py
+++ toggle-gnome-font-scale.py
@@ -7,20 +7,42 @@ gi.require_version("Gtk", "3.0")
 gi.require_version('AppIndicator3', '0.1')
 gi.require_version('Notify', '0.7')
 from gi.repository import Gtk as gtk, AppIndicator3 as appindicator, Notify as notify, GLib as glib
-from PIL import Image as imaage, ImageDraw as draw
+from PIL import Image, ImageDraw as Draw, ImageFont as Font
 from pynput import keyboard
 
 
 class Tray:
     def __init__(self):
+
+        # create scales
+        self.scales = [1.0, 1.25, 1.5]
+
+        # create icons for each scale
+        for scale in self.scales:
+            text = f"{scale}"
+            size = (96,64)
+            font_size = 36
+            image = Image.new('RGBA', size, (255, 255, 255, 0))
+            draw = Draw.Draw(image)
+            try:
+                font = Font.truetype("DejaVuSans-Bold.ttf", font_size)
+            except IOError:
+                font = Font.load_default()
+            text_size = draw.textsize(text, font=font)
+            text_position = ((size[0] - text_size[0]) // 2, (size[1] - text_size[1]) // 2)
+            draw.text(text_position, text, font=font, fill=(255, 255, 255, 255))
+
+            # Save the image to a temporary file
+            temp_icon = f'/tmp/tray_{scale}_icon.png'
+            image.save(temp_icon)
+
+
         # initiate tray app
         self.indicator = appindicator.Indicator.new("fontscale", "", appindicator.IndicatorCategory.APPLICATION_STATUS)
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
 
-        # get initial state
-        self.get_scale()
-
         # create menu based on state
+        self.update_icon()
         self.menu()
     
         # keyboard shortcut to cycle through scales
@@ -30,13 +52,42 @@ class Tray:
         self.listener.start()
         
 
+    def update_icon(self):
+        ## Generate an image with the given text
+        #text = f"{text}"
+        #print(text)
+        #size = (96,64)
+        ##size = (64, 64)
+        #font_size = 42
+        #image = Image.new('RGBA', size, (255, 255, 255, 0))
+        #draw = Draw.Draw(image)
+        #try:
+        #    font = Font.truetype("DejaVuSans-Bold.ttf", font_size)
+        #except IOError:
+        #    font = Font.load_default()
+        #text_size = draw.textsize(text, font=font)
+        #text_position = ((size[0] - text_size[0]) // 2, (size[1] - text_size[1]) // 2)
+        #draw.text(text_position, text, font=font, fill=(255, 255, 255, 255))
+
+        ## Save the image to a temporary file
+        #temp_icon = '/tmp/tray_icon.png'
+        #image.save(temp_icon)
+
+
+        # Set the generated image as the tray icon
+        #self.indicator.set_icon_full(temp_icon, text)
+        self.get_scale()
+        self.indicator.set_icon_full(f"/tmp/tray_{self.scale}_icon.png", f"{self.scale}")
+
+
     def menu(self):
         menu = gtk.Menu()
 
         # add font scales as menu options
-        scales = [1.0, 1.25, 1.5]
+        #scales = [1.0, 1.25, 1.5]
         connections = {}
-        for scale in scales:
+        #for scale in scales:
+        for scale in self.scales:
             connections[scale] = gtk.MenuItem()
             connections[scale].set_label(f"{scale}")
             connections[scale].connect('activate', self.set_scale, scale)
@@ -65,9 +116,13 @@ class Tray:
 
 
     def set_scale(self, _, scale):
-        self.scale = subprocess.Popen(["gsettings","set","org.gnome.desktop.interface","text-scaling-factor", f"{scale}"])
-        time.sleep(0.5)
-        self.get_scale()
+        subprocess.Popen(["gsettings","set","org.gnome.desktop.interface","text-scaling-factor", f"{scale}"])
+        time.sleep(0.3)
+        self.update_icon()
+        #print('a')
+        #print(self.scale)
+        #glib.timeout_add(200, self.update_icon, self.scale)
+        #print('b')
 
 
     def toggle_scale(self):
